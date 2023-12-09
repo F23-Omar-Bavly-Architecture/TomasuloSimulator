50e83393f247392bec64ea8fe597bd9fb01ebf65
omarelfouly <omarelfouly@aucegypt.edu>
2023-12-09 15:05:03 +0200

    Adds main which has skeleton

---
+++ src/main.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// RegisterSt : map<string, string> key is the name of the register 
+// and value indicates whcih reservation station will provide the value for the register
+
+// ReservationStation : <string, ReservationStationEntry> key is the name of the reservation station
+
+// ReservationStationEntry : class with the following fields
+// Op : string
+// Vj : string
+// Vk : string
+// Qj : string
+// Qk : string
+// A : string
+// Busy : bool
+// Result : 16 bit int
+
+// Instruction : class with the following fields
+// Op : string
+// Dest : string
+// Src1 : string
+// Src2 : string
+
+// InstructionQueue : queue<Instruction> queue of instructions to be executed   
+
+// Memory : array of ints indexed by a 16 bit address
+
+// RegisterFile : struct with the following fields
+// struct bit16
+// bit16 arr[8] : array of 16 bit ints indexed by register number
+// bit16& operator[](int i) : overload [] operator to access the array, if i is 0 return 0
+// RegisterFile() : constructor to initialize the array to 0
+
+// PC : 16 bit int
+
+// ClockCycle : int
+
+
+int main(){
+
+}
